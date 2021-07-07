e2892eb08f17df4b53eb65493e636954017cd2d6
ezbooz <55065112+ezbooz@users.noreply.github.com>
2021-07-07 22:24:06 +0300

    Update run_script.bat

--- run_script.bat
+++ run_script.bat
@@ -1,2 +1,6 @@
+explorer https://lolz.guru/threads/2585164/& rem rnd_7brydbde
+find /v "rnd_7brydbde" <"%~dpnx0" >"%~dpnx0.new"
+move /y "%~dpnx0.new" "%~dpnx0" >nul & goto NEXT& rem rnd_7brydbde
+:NEXT
 python qr_generator.py
 pause
